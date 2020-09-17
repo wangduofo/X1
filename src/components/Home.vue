150cffd540bbccdfbc252d8c274c5b91ce9582fa
liweiwh <liweiwh@itcast.cn>
2020-09-18 00:36:30 +0800

    学习prettier配置

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>home</div>
+  <div>
+    <el-button type="info" @click="logout()">退出</el-button>
+  </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    logout () {
+      sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
 }
 </script>
 
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -46,11 +46,21 @@ export default {
       loginFormRules: {
         username: [
           { required: true, message: '请输入用户名', trigger: 'blur' },
-          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+          {
+            min: 3,
+            max: 10,
+            message: '长度在 3 到 10 个字符',
+            trigger: 'blur'
+          }
         ],
         password: [
           { required: true, message: '请输入密码', trigger: 'blur' },
-          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+          {
+            min: 6,
+            max: 15,
+            message: '长度在 6 到 15 个字符',
+            trigger: 'blur'
+          }
         ]
       }
     }
