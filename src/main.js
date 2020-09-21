36d968d3b4135770ef6121e1ecab4b65049a4a65
liweiwh <liweiwh@itcast.cn>
2020-09-21 14:49:43 +0800

    配置axios拦截器headers中添加Authorization字段

--- src/main.js
+++ src/main.js
@@ -10,6 +10,11 @@ import '@/assets/fonts/iconfont.css'
 import axios from 'axios'
 
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1'
+axios.interceptors.request.use(config => {
+  console.log(config)
+  config.headers.Authorization = sessionStorage.getItem('token')
+  return config
+})
 
 Vue.prototype.$http = axios
 
