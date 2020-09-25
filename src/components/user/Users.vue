9985acae989f13a367d8b70cda8b5db790fb9ff4
liweiwh <liweiwh@itcast.cn>
2020-09-25 23:08:16 +0800

    删除用户接口对接

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -377,6 +377,15 @@ export default {
       if (confirmResult !== 'confirm') {
         return this.$message.info('已取消删除')
       }
+
+      const { data: res } = await this.$http.delete('users/' + id)
+
+      if (res.meta.status !== 200) {
+        return this.$message.error('删除用户失败！')
+      }
+
+      this.$message.success('删除用户成功！')
+      this.getUserList()
     }
   }
 }
