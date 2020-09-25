48f0b16f43bdaeb1b7ba4b06a64c18d79a2a5d38
liweiwh <liweiwh@itcast.cn>
2020-09-25 23:10:35 +0800

    路由 + 权限页面布局

---
+++ src/components/power/Rights.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>
+        <a href="/">权限管理</a>
+      </el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片视图 -->
+    <el-card> 123 </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
