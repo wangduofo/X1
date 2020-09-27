15528b662494610c8d6d3eaca0b8895c0918d0e3
liweiwh <liweiwh@itcast.cn>
2020-09-27 17:43:23 +0800

    使用插槽完成定义列

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -26,6 +26,32 @@
       border
       :show-row-hover="false"
     >
+      <!-- 是否有效 -->
+      <template v-slot:isok="{ row }">
+        <i
+          class="el-icon-success"
+          v-if="row.cat_deleted === false"
+          style="color: lightgreen"
+        ></i>
+        <i class="el-icon-error" v-else style="color: red"></i>
+      </template>
+      <!-- 排序 -->
+      <template #order="{ row }">
+        <el-tag size="mini" v-if="row.cat_level === 0">一级</el-tag>
+        <el-tag type="success" size="mini" v-else-if="row.cat_level === 1"
+          >二级</el-tag
+        >
+        <el-tag type="warning" size="mini" v-else>三级</el-tag>
+      </template>
+      <!-- 操作 -->
+      <template #opt>
+        <el-button type="primary" icon="el-icon-edit" size="mini"
+          >编辑</el-button
+        >
+        <el-button type="danger" icon="el-icon-delete" size="mini"
+          >删除</el-button
+        >
+      </template>
     </tree-table>
   </div>
 </template>
@@ -49,6 +75,27 @@ export default {
         {
           label: '分类名称',
           prop: 'cat_name'
+        },
+        {
+          label: '是否有效',
+          // 表示，将当前列定义为模板列
+          type: 'template',
+          // 表示当前这一列使用模板名称
+          template: 'isok'
+        },
+        {
+          label: '排序',
+          // 表示，将当前列定义为模板列
+          type: 'template',
+          // 表示当前这一列使用模板名称
+          template: 'order'
+        },
+        {
+          label: '操作',
+          // 表示，将当前列定义为模板列
+          type: 'template',
+          // 表示当前这一列使用模板名称
+          template: 'opt'
         }
       ]
     }
