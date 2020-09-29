b524addd47d2b3a5a3929db9e5f0cdebc3b4c9dd
liweiwh <liweiwh@itcast.cn>
2020-09-30 00:28:31 +0800

    通过编程式导航跳转到商品添加页面

---
+++ src/components/goods/Add.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>添加商品</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/components/goods/List.vue
+++ src/components/goods/List.vue
@@ -25,7 +25,7 @@
           </el-input>
         </el-col>
         <el-col :span="4">
-          <el-button type="primary">添加商品</el-button>
+          <el-button type="primary" @click="goAddpage">添加商品</el-button>
         </el-col>
       </el-row>
 
@@ -149,6 +149,9 @@ export default {
 
       this.$message.success('删除成功！')
       this.getGoodsList()
+    },
+    goAddpage () {
+      this.$router.push('/goods/add')
     }
   }
 }
