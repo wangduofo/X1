af47c24fa96c6a88fbbc15b333ced9f54b986054
liweiwh <liweiwh@itcast.cn>
2020-09-29 17:52:21 +0800

    分析参数页面基本结构

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import '@/assets/fonts/iconfont.css'
 
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1'
+axios.defaults.baseURL = 'http://vue-shop-api.itheima.net/api/private/v1'
 axios.interceptors.request.use(config => {
   console.log(config)
   config.headers.Authorization = sessionStorage.getItem('token')
