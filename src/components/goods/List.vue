5ef696005419fd69dca44bd66ff5ab7b58c9df9f
liweiwh <liweiwh@itcast.cn>
2020-09-30 00:13:49 +0800

    渲染商品表格数据

--- src/components/goods/List.vue
+++ src/components/goods/List.vue
@@ -19,6 +19,42 @@
           <el-button type="primary">添加商品</el-button>
         </el-col>
       </el-row>
+
+      <!-- table表格区域 -->
+      <el-table :data="goodslist" border stripe>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="商品名称" prop="goods_name"></el-table-column>
+        <el-table-column
+          label="商品价格(元)"
+          prop="goods_price"
+          width="95px"
+        ></el-table-column>
+        <el-table-column
+          label="商品重量"
+          prop="goods_weight"
+          width="70px"
+        ></el-table-column>
+        <el-table-column label="创建时间" prop="add_time" width="140px">
+          <template #default="{ row }">
+            {{ row.add_time }}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="130px">
+          <template #default="{ row }">
+            <el-button
+              type="primary"
+              icon="el-icon-edit"
+              size="mini"
+            ></el-button>
+            <el-button
+              type="danger"
+              icon="el-icon-delete"
+              size="mini"
+              @click="removeById(row.goods_id)"
+            ></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </el-card>
   </div>
 </template>
